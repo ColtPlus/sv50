88207115e30e130c8640cb880ed20969d9afd741
ColtPlus <117876256+ColtPlus@users.noreply.github.com>
2026-04-20 18:56:59 +0800

    Update start_service.sh

--- start_service.sh
+++ start_service.sh
@@ -2,14 +2,12 @@
 gpioset 4 19=1
 #led_r
 gpioset 2 16=1
-#usbdp_phy0
-gpioset 0 22=1
-gpioset 0 27=1
-#usb5v_h_en
-gpioset 4 8=1
-#usb5v_o_en
-gpioset 4 3=1
-#usbc_rst
-gpioset 3 14=1
-#usbc_det
-gpioset 3 29=1
+
+#USB30_PWREN_A
+gpioset 4 27=1
+#USB30_PWREN_B
+gpioset 1 25=1
+#USB20_PWREN_A
+gpioset 4 26=1
+#WIFI_PWREN
+gpioset 2 8=1
